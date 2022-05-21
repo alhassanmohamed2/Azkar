6fe43d2880282a2ba157be9f89f084b0fac8978f
Alhassan Mohamed <65092971+alhassanmohamed2@users.noreply.github.com>
2022-05-21 05:42:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/icon.png" type="image/x-icon">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css?v=<?= time() ?>">
+    <link rel="stylesheet" href="css/style.css">
    <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Scheherazade&display=swap" rel="stylesheet">
 
@@ -206,7 +206,7 @@
 
 
 </body>
-<script src="script/script.js?v=<?= time() ?>"></script>
+<script src="script/script.js"></script>
 
 
 </html>
